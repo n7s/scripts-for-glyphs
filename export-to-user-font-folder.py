17d5da068d49021e3e10778a340cca0dc4af6ce7
Nicolas Spalinger <nicolas.spalinger@sil.org>
2017-01-30 11:06:52 +0100

    Adjusted path var.

--- export-to-user-font-folder.py
+++ export-to-user-font-folder.py
@@ -8,10 +8,10 @@ __copyright__ = 'Copyright (c) 2017, SIL International  (http://www.sil.org)'
 __license__ = 'Released under the MIT License (http://opensource.org/licenses/MIT)'
 __author__ = 'Nicolas Spalinger.'
 
-path = os.path.expanduser('~/Library/Fonts')
+exportFolder = os.path.expanduser('~/Library/Fonts')
 
 
 for instance in Glyphs.font.instances:
         instance.generate(FontPath = exportFolder)
 
-Glyphs.showNotification('Exporting OpenType fonts to user font folder', '%s %s.%s\n%s' % (Glyphs.font.familyName, Glyphs.font.versionMajor, Glyphs.font.versionMinor, path))
+Glyphs.showNotification('Exporting OpenType fonts to user font folder', '%s %s.%s\n%s' % (Glyphs.font.familyName, Glyphs.font.versionMajor, Glyphs.font.versionMinor, exportFolder))
