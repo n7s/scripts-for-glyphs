3bece3327e497559088ca6af6d146df64cd7bd38
Nicolas Spalinger <nicolas.spalinger@sil.org>
2018-04-25 15:25:06 +0200

    Adjusted params to get pysilfont from release. Turned off pip version checking.

--- update-preflight-libraries-from-releases.py
+++ update-preflight-libraries-from-releases.py
@@ -1,4 +1,4 @@
-#MenuTitle: Update the preflight libraries from releases (pysilfont from master)
+#MenuTitle: Update the preflight libraries from releases
 # -*- coding: utf-8 -*-
 __doc__="""
 Update the preflight libraries from releases
@@ -31,7 +31,7 @@ tell application "Finder"
 
 		tell window 1
 
-			do script "sudo -H  /usr/local/bin/pip install --upgrade --user git+https://github.com/silnrsi/pysilfont;  sudo -H  /usr/local/bin/pip install --upgrade --user glyphsLib;  sudo -H  /usr/local/bin/pip install  --upgrade --user fontTools ;  sudo -H  /usr/local/bin/pip install  --upgrade --user MutatorMath ;  sudo -H  /usr/local/bin/pip install  --upgrade --user ufoLib ;  sudo -H  /usr/local/bin/pip install --upgrade  --user defcon ;  sudo -H  /usr/local/bin/pip install  --upgrade --user fontMath "
+			do script "sudo -H  /usr/local/bin/pip install --disable-pip-version-check --upgrade --user git+https://github.com/silnrsi/pysilfont.git@v1.3.0#egg=pysilfont;  sudo -H  /usr/local/bin/pip install --disable-pip-version-check --upgrade --user glyphsLib;  sudo -H  /usr/local/bin/pip  install --disable-pip-version-check  --upgrade --user fontTools ;  sudo -H  /usr/local/bin/pip install --disable-pip-version-check  --upgrade --user MutatorMath ;  sudo -H  /usr/local/bin/pip  install --disable-pip-version-check  --upgrade --user ufoLib ;  sudo -H  /usr/local/bin/pip install --disable-pip-version-check --upgrade  --user defcon ;  sudo -H  /usr/local/bin/pip install --disable-pip-version-check  --upgrade --user fontMath ;  pip list --format=columns --disable-pip-version-check --pre"
 
 		end tell
 
