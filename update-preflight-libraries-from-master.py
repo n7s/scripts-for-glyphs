251035a8dd29b803cca5c3a48cb7d7554b43db09
Nicolas Spalinger <nicolas.spalinger@sil.org>
2018-06-15 23:19:39 +0200

    Streamlined the update scripts.

--- update-preflight-libraries-from-master.py
+++ update-preflight-libraries-from-master.py
@@ -31,12 +31,17 @@ tell application "Finder"
 
 		tell window 1
 
-			do script "sudo -H  /usr/local/bin/pip install --upgrade --user  git+https://github.com/silnrsi/pysilfont.git@master#egg=pysilfont;  sudo -H  /usr/local/bin/pip install --upgrade --user git+https://github.com/googlei18n/glyphsLib.git@master#egg=glyphsLib;  sudo -H  /usr/local/bin/pip install  --upgrade --user git+https://github.com/fonttools/fonttools.git@master#egg=fontTools ;  sudo -H  /usr/local/bin/pip install  --upgrade --user git+https://github.com/LettError/MutatorMath.git@master#egg=MutatorMath ;  sudo -H  /usr/local/bin/pip install  --upgrade --user git+https://github.com/unified-font-object/ufoLib.git@master#egg=ufoLib ;  sudo -H  /usr/local/bin/pip install --upgrade  --user git+https://github.com/typesupply/defcon.git@master#egg=defcon ;  sudo -H  /usr/local/bin/pip install  --upgrade --user git+https://github.com/typesupply/fontMath.git@master#egg=fontMath "
+			do script "sudo -H  /usr/local/bin/pip install --upgrade --user --no-warn-script-location git+https://github.com/silnrsi/pysilfont.git@master#egg=pysilfont git+https://github.com/googlei18n/glyphsLib.git@master#egg=glyphsLib git+https://github.com/fonttools/fonttools.git@master#egg=fontTools  git+https://github.com/LettError/MutatorMath.git@master#egg=MutatorMath  git+https://github.com/unified-font-object/ufoLib.git@master#egg=ufoLib git+https://github.com/typesupply/defcon.git@master#egg=defcon  git+https://github.com/typesupply/fontMath.git@master#egg=fontMath  git+https://github.com/LettError/DesignSpaceDocument.git@master#egg=DesignSpaceDocument "
+
 
 		end tell
 
 	end tell
 
+end tell
+
+tell application "Finder"
+	display notification "Updating your preflight dependencies, please enter your user password" with title "Preflight dependencies update" sound name "default"
 end tell
 
 
--- update-preflight-libraries-from-releases.py
+++ update-preflight-libraries-from-releases.py
@@ -31,12 +31,16 @@ tell application "Finder"
 
 		tell window 1
 
-			do script "sudo -H  /usr/local/bin/pip install --disable-pip-version-check --upgrade --user git+https://github.com/silnrsi/pysilfont.git@v1.3.0#egg=pysilfont;  sudo -H  /usr/local/bin/pip install --disable-pip-version-check --upgrade --user glyphsLib;  sudo -H  /usr/local/bin/pip  install --disable-pip-version-check  --upgrade --user fontTools ;  sudo -H  /usr/local/bin/pip install --disable-pip-version-check  --upgrade --user MutatorMath ;  sudo -H  /usr/local/bin/pip  install --disable-pip-version-check  --upgrade --user ufoLib ;  sudo -H  /usr/local/bin/pip install --disable-pip-version-check --upgrade  --user defcon ;  sudo -H  /usr/local/bin/pip install --disable-pip-version-check  --upgrade --user fontMath ;  pip list --format=columns --disable-pip-version-check --pre"
+			do script "sudo -H  /usr/local/bin/pip install --no-warn-script-location --disable-pip-version-check --upgrade --user git+https://github.com/silnrsi/pysilfont.git@v1.3.0#egg=pysilfont glyphsLib fontTools MutatorMath ufoLib defcon fontMath git+https://github.com/LettError/DesignSpaceDocument.git@master#egg=DesignSpaceDocument ;  pip list --format=columns --disable-pip-version-check --user "
 
 		end tell
 
 	end tell
 
+end tell
+
+tell application "Finder"
+	display notification "Updating your preflight dependencies, please enter your user password" with title "Preflight dependencies update" sound name "default"
 end tell
 
 
